885e6b92e62f33c44475c244f0d2c0eb83566cce
Nirupam Rabha <incrediblia52@gmail.com>
2025-10-31 11:57:39 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,12 +2,17 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
-export default defineConfig({
-  base: "/tasty-kitchen-website/", // ✅ must match repo name exactly
+// ✅ Make sure this base path exactly matches your GitHub repo name:
+export default defineConfig(({ mode }) => ({
+  base: mode === "production" ? "/tasty-kitchen-website/" : "/",
+  server: {
+    host: "::",
+    port: 8080,
+  },
   plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-});
+}));
